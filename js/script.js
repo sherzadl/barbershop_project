74329509b187ae9ae4763ffc0158b61d8ea46eb8
sherzadl <s.pishenbaev@newuu.uz>
2025-02-25 21:18:33 +0500

    barbershop

--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
-// Logo Animation
-window.addEventListener('load', () => {
-    const logo = document.querySelector('.logo');
-    logo.classList.add('logo-animate');
+// Get elements
+const gallery = document.querySelector('.gallery');
+const pageContainer = document.querySelector('.page-container');
+const barbershopImage = document.querySelector('.gallery-item img[alt="Barbershop"]');
 
-    // Trigger gallery animation for all at once
-    const galleryItems = document.querySelectorAll('.gallery-item');
-    galleryItems.forEach((item) => {
-        item.style.opacity = '1';
-        item.style.transform = 'scale(1)';
-    });
+// Event Listener to Show Pages on Click
+barbershopImage.addEventListener('click', () => {
+    gallery.classList.add('hide-gallery');
+    setTimeout(() => {
+        pageContainer.classList.add('show-pages');
+    }, 500);
 });
-    
