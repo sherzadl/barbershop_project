31a838497a464cdb2cfa6a057ce89147f8c923c0
sherzadl <s.pishenbaev@newuu.uz>
2025-02-25 23:33:30 +0500

    page5

--- js/script.js
+++ js/script.js
@@ -41,3 +41,12 @@ document.querySelector('.left-arrow').addEventListener('click', function() {
 document.querySelector('.right-arrow').addEventListener('click', function() {
     alert('Right arrow clicked!');
 });
+// Smooth scroll to Page 5 after scrolling Page 4
+window.addEventListener('scroll', function() {
+    const page4 = document.querySelector('.page4');
+    const page5 = document.querySelector('.page5');
+
+    if (page4.getBoundingClientRect().bottom < window.innerHeight) {
+        page5.scrollIntoView({ behavior: 'smooth' });
+    }
+});
